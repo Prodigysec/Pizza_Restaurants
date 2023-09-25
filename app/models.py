f56bd69879dcd7314af552c952c6ccfb2594ea67
Ian Raburu <williamraburu@gmail.com>
2023-09-25 08:34:11 +0300

    Add database models

--- app/models.py
+++ app/models.py
@@ -10,3 +10,47 @@ metadata = MetaData(naming_convention={
 # SQLAlchemy configuration
 db = SQLAlchemy(metadata=metadata)
 
+class RestaurantPizza(db.Model, SerializerMixin):
+    __tablename__ = 'restaurant_pizzas'
+
+    serialize_rules = ('-restaurant.restaurant_pizzas', '-pizza.restaurant_pizzas',) # relationship.related_model
+
+    id = db.Column(db.Integer, primary_key=True)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    
+
+    restaurant = db.relationship('Restaurant', backref='restaurant_pizzas')
+    pizza = db.relationship('Pizza', backref='restaurant_pizzas')
+
+
+class Restaurant(db.Model, SerializerMixin):
+    __tablename__ = 'restaurants'
+
+    serialize_rules = ('-restaurantPizza.restaurants', '-pizza.restaurants',) # relationship.related_model
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    address = db.Column(db.String)
+
+    pizza = db.relationship('Pizza', secondary='restaurant_pizzas', back_populates='restaurants')
+    restaurantPizza = db.relationship('RestaurantPizza', back_populates='restaurant')
+
+
+class Pizza(db.Model, SerializerMixin):
+    __tablename__ = 'pizzas'
+
+    serialize_rules = ('-restaurant.pizzas', '-restaurantPizza.restaurant',) # relationship.related_model
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    description = db.Column(db.String)
+    price = db.Column(db.Integer)
+
+    restaurant = db.relationship('Restaurant', secondary='restaurant_pizzas', back_populates='pizzas')
+    restaurantPizza = db.relationship('RestaurantPizza', back_populates='pizza')
+
+    
